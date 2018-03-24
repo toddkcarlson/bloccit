8c9e2fc991c71f3df87877d9e468b6ef89acdd08
Todd Carlson <tcarlson@carlsonsw.com>
2018-03-24 12:15:02 -0400

    assignment 14 model

---
+++ spec/models/question_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+
+RSpec.describe Question, type: :model do
+ # #1
+   let(:question) { Question.create!(title: "New Question Title", body: "New Question Body") }
+ 
+ # #2
+   describe "attributes" do
+     it "has title and body attributes" do
+       expect(question).to have_attributes(title: "New Question Title", body: "New Question Body")
+     end
+   end
+
+end
+
