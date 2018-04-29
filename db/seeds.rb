614cfe66fb849037a4b0b6325779bc487af68617
Todd Carlson <tcarlson@carlsonsw.com>
2018-04-29 18:39:18 -0400

    assignment 20 topics

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,6 +29,15 @@
      body: RandomData.random_paragraph
    )
  end
+
+30.times do
+    SponsoredPost.create!(
+        topic: topics.sample,
+        title: RandomData.random_sentence,
+        body: RandomData.random_paragraph,
+        price: RandomData.random_number
+    )
+end
  
  unique_post = Post.find_or_create_by!(title: "Todd Title", body: "Toddy Body")
  Comment.find_or_create_by!(post: unique_post, body:"No comment")
@@ -36,4 +45,5 @@
  puts "Seed finished"
  puts "#{Topic.count} topics created"
  puts "#{Post.count} posts created"
+ puts "#{SponsoredPost.count} sponsored posts created"
  puts "#{Comment.count} comments created"
