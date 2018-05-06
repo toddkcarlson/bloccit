145f529a45a2c71b0662531dd8fc2bb2bf0a00da
Todd Carlson <tcarlson@carlsonsw.com>
2018-05-06 14:04:03 -0400

    assignment 23 auth, spec_helper issue

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,4 @@
-require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe User, type: :model do
   let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "password") }
