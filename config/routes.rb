f519bc132a4c7c378c4c6fc4a687d0a488c09fe8
Todd Carlson <tcarlson@carlsonsw.com>
2018-05-09 00:09:01 -0400

    checkpoint 25 sessions

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
      resources :posts, except: [:index]
    end
 
-   resources :users, only: [:new, :create]   
+   resources :users, only: [:new, :create]
+
+   resources :sessions, only: [:new, :create, :destroy]      
   
    get 'about' => 'welcome#about'
 
