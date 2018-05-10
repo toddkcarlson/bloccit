de33f533653da57135db3aa3f5db603320f24cd9
Todd Carlson <tcarlson@carlsonsw.com>
2018-05-10 00:02:34 -0400

    added topic form partial

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,3 +1,6 @@
 class Topic < ApplicationRecord
    has_many :posts, dependent: :destroy
+
+   validates :name, length: (minimun: 5 ), presence: true
+   validates :description, length: (minimun: 15 ), presence: true
 end
