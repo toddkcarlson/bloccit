abae0c4e9c32582f2b03bbe613a363e338daf9ec
Todd Carlson <tcarlson@carlsonsw.com>
2018-05-18 23:02:34 -0400

    assignment 29 vote

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,8 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :votes, dependent: :destroy
 
+  after_create :create_vote
+
   default_scope { order('rank DESC') }
 
   validates :title, length: { minimum: 5 }, presence: true
@@ -31,4 +33,8 @@ class Post < ApplicationRecord
      new_rank = points + age_in_days
      update_attribute(:rank, new_rank)
    end
+
+   def create_vote
+    user.votes.create(value: 1, post: self)
+  end
 end
