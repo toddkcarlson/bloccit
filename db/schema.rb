5cc6db4202b024ceaf4647760dac134fb8fbb6f7
Todd Carlson <tcarlson@carlsonsw.com>
2018-05-18 22:25:37 -0400

    checkpoint 29 vote

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180515032922) do
+ActiveRecord::Schema.define(version: 20180519014954) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20180515032922) do
     t.datetime "updated_at", null: false
     t.integer "topic_id"
     t.integer "user_id"
+    t.float "rank"
     t.index ["topic_id"], name: "index_posts_on_topic_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
@@ -48,6 +49,16 @@ ActiveRecord::Schema.define(version: 20180515032922) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "role"
+  end
+
+  create_table "votes", force: :cascade do |t|
+    t.integer "value"
+    t.integer "user_id"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_votes_on_post_id"
+    t.index ["user_id"], name: "index_votes_on_user_id"
   end
 
 end
